689c6ae831914f495381b299d49c19290b8c8593
Mariano <mariano.palliser@gmail.com>
2017-02-04 10:22:03 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 window.onload = function(){
+ 
 var xhttp = new XMLHttpRequest();
-
 xhttp.onreadystatechange = function() {
  if (this.readyState == 4 && this.status == 200) {
   gestionarXml(this);
@@ -8,9 +8,13 @@ xhttp.onreadystatechange = function() {
 };
 xhttp.open("GET", "xml.xml", true);
 xhttp.send();
+}
 
 function gestionarXml(dadesXml){
-var xmlDoc = dadesXml.responseXML;
-document.getElementById("p1").innerHTML = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+ var xmlDoc = dadesXml.responseXML;
+ var tituloInput = xmlDoc.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+}
+ function ponerDatosInputHtml(t){
+ document.getElementById("tituloInput").innerHTML = t;
 
 }
